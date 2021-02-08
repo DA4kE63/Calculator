4562e15589f2b8eb4c0a0f2109130078139f51d5
FÆÎSAL  DA4K E63 <78696114+DA4kE63@users.noreply.github.com>
2021-02-08 16:01:23 +0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,36 @@
+print("hello guys how are all  guys im now one make tool and \n welcome to my tool guys \n help me abu huraira bro for help tnx" )
+print("today i will show u one calculeter for biginer python student")
+print("""
+[1]plus
+[2]minus
+[3]into
+[4]division
+[5]hacker
+[crackers]
+[spammers]""")
+option= input("enter your option ")
+if option == "1" :
+    num1= float (input("enter your number :"))
+    num2=float (input("enter your number:"))
+    print( "your result its:",num1+num2)
+elif option == "2":
+	num1=float(input("enter a number :"))
+	num2=float (input("enter a number:"))
+	print("your result",num1-num2)
+elif option == "3":
+	num1=float(input("enter a number :"))
+	num2=float (input("enter a number:"))
+	print("your result",num1*num2)
+elif option == "4":
+	num1=float(input("enter a number :"))
+	num2=float (input("enter a number:"))
+	print("your result",num1/num2)
+elif option == "5":
+	hacker= "faisal dark eye no pickme no skecth me hhahaha bcz im a           hacker"
+	print("im one hacker",hacker)
+
+
+print("your option wrong ")
+print ("guys this is first time coding and sorry for my bad coding i m one biginer and ")
+print ("guys ay prothom bar coding a akta jinish make korlam and sobay duya koyren insallah ami akdin valo sikhbo")
+print("pls guys like and subscribe my tool hahahha")
